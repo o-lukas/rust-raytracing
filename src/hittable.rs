c9f85322c64a374382c46d822dd58f93e0eb8e16
Lukas Obermeier <meteor.kiwi-0i@icloud.com>
2024-05-10 13:02:46 +0000

    chapter 6.5

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use nalgebra::Vector3;
 
 use crate::ray::Ray;
@@ -41,5 +43,41 @@ impl HitRecord {
         if !self.front_face {
             self.normal = -self.normal;
         }
+    }
+}
+
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        Self {
+            objects: Vec::new(),
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut closest_so_far = t_max;
+        let mut rec: Option<HitRecord> = None;
+
+        for object in self.objects.iter() {
+            if let Some(temp_rec) = object.hit(r, t_min, closest_so_far) {
+                closest_so_far = temp_rec.t;
+                rec = Some(temp_rec);
+            }
+        }
+
+        return rec;
     }
 }
