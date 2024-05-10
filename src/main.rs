f7dd696ac9569c6a05482c8c2900783ccc77eb64
Lukas Obermeier <meteor.kiwi-0i@icloud.com>
2024-05-10 10:40:23 +0000

    chapter 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+
+    println!("P3\n{}\n{}\n255\n", image_width, image_height);
+
+    for j in (0..image_height - 1).rev() {
+        for i in 0..image_width {
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
