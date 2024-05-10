8c97993eb926305ef057ce8678e469cdd0f1c215
Lukas Obermeier <meteor.kiwi-0i@icloud.com>
2024-05-10 11:01:09 +0000

    chapter 4.2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod ray;
 use std::fs;
 
 use nalgebra::Vector3;
+use ray::Ray;
 
 fn color(r: f32, g: f32, b: f32) -> Vector3<f32> {
     Vector3::new(r, g, b)
@@ -17,11 +18,28 @@ fn write_color(pixel_color: &Vector3<f32>) -> String {
     )
 }
 
+fn ray_color(r: &Ray) -> Vector3<f32> {
+    let unit_direction = r.direction().normalize();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0)
+}
+
 fn main() {
     // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f32 / aspect_ratio) as i32;
 
-    let image_width = 256;
-    let image_height = 256;
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vector3::new(0.0, 0.0, 0.0);
+    let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0.0, 0.0, focal_length);
 
     // Render
     let mut image_lines = vec![
@@ -35,11 +53,13 @@ fn main() {
         println!("Scanlines remaining: {}", j);
 
         for i in 0..image_width {
-            let pixel_color = color(
-                i as f32 / (image_width - 1) as f32,
-                j as f32 / (image_height - 1) as f32,
-                0.25,
+            let u = i as f32 / (image_width - 1) as f32;
+            let v = j as f32 / (image_height - 1) as f32;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(&r);
             image_lines.push(write_color(&pixel_color));
         }
     }
